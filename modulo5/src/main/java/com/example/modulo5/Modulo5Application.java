eaf8704a91ad256843b2afdf212d7dab4666f9e1
Lady Laura Tucuman Alarcon <tucumanalarconladylaura@gmail.com>
2018-08-03 22:08:48 -0400

    first commit

---
+++ modulo5/src/main/java/com/example/modulo5/Modulo5Application.java
@@ -0,0 +1,14 @@
+package com.example.modulo5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Modulo5Application {
+
+	public static void main(String[] args) {
+		System.out.println("<<<<HOLA>>>>");
+		SpringApplication.run(Modulo5Application.class, args);
+
+	}
+}
